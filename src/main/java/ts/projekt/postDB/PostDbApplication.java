8539b63ace8a08750c2446f47e583afe1f6aee6c
sabre <djsoundnr1@gmail.com>
2021-05-19 17:58:13 +0200

    Initial commit

---
+++ src/main/java/ts/projekt/postDB/PostDbApplication.java
@@ -0,0 +1,13 @@
+package ts.projekt.postDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ts/projekt/postDB/PostDbApplicationTests.java
@@ -0,0 +1,13 @@
+package ts.projekt.postDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
