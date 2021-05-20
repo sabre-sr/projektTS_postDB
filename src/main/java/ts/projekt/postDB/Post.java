55e256748b76bbde1e8bae26eb9e4371465e812b
sabre <djsoundnr1@gmail.com>
2021-05-20 11:47:05 +0200

    set up server post

---
+++ src/main/java/ts/projekt/postDB/Post.java
@@ -0,0 +1,49 @@
+package ts.projekt.postDB;
+
+import java.util.Date;
+
+public class Post {
+    public User author;
+    private int id;
+    private String postBody;
+    private Date creationDate;
+
+    public Post(User author, int id, String postBody, Date creationDate) {
+        this.author = author;
+        this.id = id;
+        this.postBody = postBody;
+        this.creationDate = creationDate;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getPostBody() {
+        return postBody;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPostBody(String postBody) {
+        this.postBody = postBody;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+}
